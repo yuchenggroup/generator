d7a2518da86f6ededb8e72826a3437af08a5bc8c
jefferchang <changyibiao126@163.com>
2015-11-12 20:21:09 +0800

    添加java生成插件

---
+++ core/mybatis-generator-core/src/main/java/org/mybatis/generator/api/dom/java/JavaController.java
@@ -0,0 +1,215 @@
+package org.mybatis.generator.api.dom.java;
+
+import java.util.*;
+
+import static org.mybatis.generator.api.dom.OutputUtilities.calculateImports;
+import static org.mybatis.generator.api.dom.OutputUtilities.javaIndent;
+import static org.mybatis.generator.api.dom.OutputUtilities.newLine;
+import static org.mybatis.generator.internal.util.StringUtility.stringHasValue;
+
+/**
+ * Created by jeffer on 2015/11/12.
+ */
+public class JavaController  extends JavaElement implements CompilationUnit {
+
+    private Set<FullyQualifiedJavaType> importedTypes;
+
+    private Set<String> staticImports;
+
+    private FullyQualifiedJavaType type;
+
+    private Set<FullyQualifiedJavaType> superClassTypes;
+
+    private List<Method> methods;
+
+    private List<String> fileCommentLines;
+
+    /**
+     *
+     */
+    public JavaController(FullyQualifiedJavaType type) {
+        super();
+        this.type = type;
+        superClassTypes = new LinkedHashSet<FullyQualifiedJavaType>();
+        methods = new ArrayList<Method>();
+        importedTypes = new TreeSet<FullyQualifiedJavaType>();
+        fileCommentLines = new ArrayList<String>();
+        staticImports = new TreeSet<String>();
+    }
+
+    public JavaController(String type) {
+        this(new FullyQualifiedJavaType(type));
+    }
+
+    public Set<FullyQualifiedJavaType> getImportedTypes() {
+        return Collections.unmodifiableSet(importedTypes);
+    }
+
+    public void addImportedType(FullyQualifiedJavaType importedType) {
+        if (importedType.isExplicitlyImported()
+                && !importedType.getPackageName().equals(type.getPackageName())) {
+            importedTypes.add(importedType);
+        }
+    }
+
+    public String getFormattedContent() {
+        StringBuilder sb = new StringBuilder();
+
+        for (String commentLine : fileCommentLines) {
+            sb.append(commentLine);
+            newLine(sb);
+        }
+
+        if (stringHasValue(getType().getPackageName())) {
+            sb.append("package "); //$NON-NLS-1$
+            sb.append(getType().getPackageName());
+            sb.append(';');
+            newLine(sb);
+            newLine(sb);
+        }
+
+        for (String staticImport : staticImports) {
+            sb.append("import static "); //$NON-NLS-1$
+            sb.append(staticImport);
+            sb.append(';');
+            newLine(sb);
+        }
+
+        if (staticImports.size() > 0) {
+            newLine(sb);
+        }
+
+        Set<String> importStrings = calculateImports(importedTypes);
+        for (String importString : importStrings) {
+            sb.append(importString);
+            newLine(sb);
+        }
+
+        if (importStrings.size() > 0) {
+            newLine(sb);
+        }
+
+        int indentLevel = 0;
+
+        addFormattedJavadoc(sb, indentLevel);
+        addFormattedAnnotations(sb, indentLevel);
+
+        sb.append(getVisibility().getValue());
+
+        if (isStatic()) {
+            sb.append("static "); //$NON-NLS-1$
+        }
+
+        if (isFinal()) {
+            sb.append("final "); //$NON-NLS-1$
+        }
+
+        sb.append("class "); //$NON-NLS-1$
+        sb.append(getType().getShortName());
+
+        if (getSuperClassTypes().size() > 0) {
+            sb.append(" extends "); //$NON-NLS-1$
+
+            boolean comma = false;
+            for (FullyQualifiedJavaType fqjt : getSuperClassTypes()) {
+                if (comma) {
+                    sb.append(", "); //$NON-NLS-1$
+                } else {
+                    comma = true;
+                }
+
+                sb.append(fqjt.getShortName());
+            }
+        }
+
+        sb.append(" {"); //$NON-NLS-1$
+        indentLevel++;
+
+        Iterator<Method> mtdIter = getMethods().iterator();
+        while (mtdIter.hasNext()) {
+            newLine(sb);
+            Method method = mtdIter.next();
+            sb.append(method.getFormattedContent(indentLevel, false));
+            if (mtdIter.hasNext()) {
+                newLine(sb);
+            }
+        }
+
+        indentLevel--;
+        newLine(sb);
+        javaIndent(sb, indentLevel);
+        sb.append('}');
+
+        return sb.toString();
+    }
+
+    public void getSuperClass(FullyQualifiedJavaType superClass) {
+        superClassTypes.add(superClass);
+    }
+
+    /**
+     * @return Returns the methods.
+     */
+    public List<Method> getMethods() {
+        return methods;
+    }
+
+    public void addMethod(Method method) {
+        methods.add(method);
+    }
+
+    /**
+     * @return Returns the type.
+     */
+    public FullyQualifiedJavaType getType() {
+        return type;
+    }
+
+    public FullyQualifiedJavaType getSuperClass() {
+
+
+        return null;
+    }
+
+    @Override
+    public Set<FullyQualifiedJavaType> getSuperInterfaceTypes() {
+        return null;
+    }
+
+    public Set<FullyQualifiedJavaType> getSuperClassTypes() {
+        return superClassTypes;
+    }
+
+    public boolean isJavaInterface() {
+        return false;
+    }
+
+    public boolean isJavaEnumeration() {
+        return false;
+    }
+
+    public void addFileCommentLine(String commentLine) {
+        fileCommentLines.add(commentLine);
+    }
+
+    public List<String> getFileCommentLines() {
+        return fileCommentLines;
+    }
+
+    public void addImportedTypes(Set<FullyQualifiedJavaType> importedTypes) {
+        this.importedTypes.addAll(importedTypes);
+    }
+
+    public Set<String> getStaticImports() {
+        return staticImports;
+    }
+
+    public void addStaticImport(String staticImport) {
+        staticImports.add(staticImport);
+    }
+
+    public void addStaticImports(Set<String> staticImports) {
+        this.staticImports.addAll(staticImports);
+    }
+
+}
